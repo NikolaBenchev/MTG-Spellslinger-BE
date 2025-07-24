e0559cd8a99d8b6e12155a634ee8d4c7a229bb09
NikolaBenchev <koliobenchev2@gmail.com>
2025-07-25 00:37:46 +0300

    KAN-35 initial backend setup with slimphp, predis and composer

---
+++ phinx.php
@@ -0,0 +1,32 @@
+<?php
+
+return
+    [
+        'paths' => [
+            'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+            'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+        ],
+        'environments' => [
+            'default_migration_table' => 'phinxlog',
+            'default_environment' => 'development',
+            'production' => [
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'name' => 'mtg_spellslinger',
+                'user' => 'root',
+                'pass' => '',
+                'port' => '3306',
+                'charset' => 'utf8',
+            ],
+            'development' => [
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'name' => 'mtg_spellslinger_dev',
+                'user' => 'root',
+                'pass' => '',
+                'port' => '3306',
+                'charset' => 'utf8',
+            ],
+        ],
+        'version_order' => 'creation'
+    ];
--- public/index.php
+++ public/index.php
@@ -1,4 +1,20 @@
 <?php
-    echo "Hello World!";
 
-?>
+use Slim\Factory\AppFactory;
+use Predis\Client as PredisClient;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$r = new PredisClient([
+    'scheme'   => 'tcp',
+    'host'     => '127.0.0.1',
+    'port'     => 6379,
+    'password' => '',
+    'database' => 0,
+]);
+
+require __DIR__ . '/../routes.php';
+
+$app->run();
